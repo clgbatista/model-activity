63f74e6fc520b276dc1f77084a7bcba1632d17b4
Carlos Batista <carlos.batista@ieee.org>
2021-05-18 17:12:52 -0300

    commenting the and explaining the functions

--- code/modelexam.py
+++ code/modelexam.py
@@ -1,4 +1,17 @@
 # modelexam.py
+# @clgbatista - May 2021
+
+# @function change_mode(str actual_state, int config_vector[5])
+#            function to control the changes on the operational modes of a satellite
+# @params::actual_state
+#            variable that determines the actual state of the operational mode
+# @params::config_vector
+#            configuration vector with the entries and conditions of different measures or signal.
+#            config_vector[0] - batt - battery cappacity in [%]
+#            config_vector[1] - ang_rate - angular rate estimation from the onboard gyroscopes [°/min]
+#            config_vector[2] - down_flag - doownlink flag: takes the value 0 or 1 based if the GS is visible or not
+#            config_vector[3] - img_flag - imaging flag: takes the value 0 or 1 based if the imaging area is visible or not
+#            config_vector[4] - error_flag - error flag: takes the value 0 or 1 based on errors in the ADCS.
 
 def change_mode(actual_state,config_vector):
     
@@ -9,7 +22,7 @@ def change_mode(actual_state,config_vector):
     img_flag = config_vector[3]
     error_flag = config_vector[4]
     
-# SAFE mode behaviour (HIGH )
+# SAFE mode behaviour (HIGHEST priority)
     if (error_flag or batt <= 70):
         return 'SAFE'
     else:
@@ -41,14 +54,22 @@ def change_mode(actual_state,config_vector):
             else:
                 return 'DOWNLINK'
 
-# CAM mode behaviour
+# CAM mode behaviour (LOWEST priority)
         elif (actual_state == 'CAM'):
             if (not img_flag):
                 return 'IDLE'
             else:
                 return 'CAM'
 
-def pointing(actual_state,error_flag):
+# @function pointing(str actual_state, int error_flag)
+#            function to control the pointing of a satellite at the different operational modes
+# @params::actual_state
+#            variable that determines the actual state of the operational mode
+# @params::error_flag
+#            error flag takes the value 0 or 1 based on errors in the ADCS
+           
+def pointing(actual_state,error_flag):    
+# error flag has the HIGHEST priority
     if (not error_flag):
         if (actual_state == 'IDLE' or actual_state == 'SAFE'):
             return 'SUN'
@@ -56,7 +77,7 @@ def pointing(actual_state,error_flag):
             return 'NADIR'
         elif (actual_state == 'DOWNLINK'):
             return 'ECI'
-        elif (actual_state == 'DETUMBLING'):
+        elif (actual_state == 'DETUMBLING'): # pointing is not applicable (NA)
             return 'NA'
     else:
         return 'ADCS FAULT'
