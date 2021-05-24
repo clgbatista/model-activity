4bb8b88516fb51050b34b9d574a241aef68b7ce1
Carlos Batista <carlos.batista@ieee.org>
2021-05-24 09:41:32 -0300

    import modules

--- code/test_cases_fi.py
+++ code/test_cases_fi.py
@@ -1,11 +1,14 @@
 # testing_cases.py
 # @clgbatista - May 2021
 
+import pytest
+from modelexam import change_mode,pointing
+
 # SCENARIO: The satellite as operational mode, wants to not change mode, in order to not allow trasitions.
 @pytest.mark.parametrize("batt,ang_rate,down_flag,img_flag,error_flag",[
     (100,0,1,0,0),
 ])
-def test_fi_detumbling(actual_state,batt,ang_rate,down_flag,img_flag,error_flag):
+def test_fi_detumbling(batt,ang_rate,down_flag,img_flag,error_flag):
 # GIVEN: the sattelite is in DETUMBLING mode
     actual_state = 'DETUMBLING'
 # WHEN: downlink flag is true
